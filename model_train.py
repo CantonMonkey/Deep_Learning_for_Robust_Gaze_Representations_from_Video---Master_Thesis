bcf27ce0918be71a7b856100fc498f4ee881cc06
CantonMonkey <154944245+CantonMonkey@users.noreply.github.com>
2025-04-01 12:37:25 +0200

    wandb configuration

--- model_train.py
+++ model_train.py
@@ -6,6 +6,9 @@ import os
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
+import wandb # https://docs.wandb.ai/tutorials/experiments/
+
+
 
 
 
@@ -70,28 +73,152 @@ def angular_error(pred, target):
     return torch.acos(cos_sim) * (180.0 / torch.pi)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = WholeModel().to(device)
+model = WholeModel().to(device)  # Load the model
 
+# /data/leuven/374/vsc37415/OP/
 dataset_path = "C:/Users/rohan/Desktop/Master/Master Thesis/Master-Thesis/Dataset-Test/Output Folder/webcam_r"
 label_excel = "C:/Users/rohan/Desktop/Master/Master Thesis/Master-Thesis/Dataset-Test/Output Folder/data.csv"
 
 dataset = GazeDatasetFromPaths(dataset_path, label_excel)
 dataloader = DataLoader(dataset, batch_size=1, shuffle=True) # shuffle true? yes, cause label is included so no issue
 
-def train():
-    model = WholeModel()
 
-    bs = 16 # batch size
-    # hidden_size =
-    # sequence_length =
-    # num_classes =
+def validate_model(model, valid_dl, loss_func, log_images=False, batch_idx=0):
+    "Compute performance of the model on the validation dataset and log a wandb.Table"
+    model.eval()
+    val_loss = 0.0
+    with torch.inference_mode():
+        correct = 0
+        for i, (images, labels) in enumerate(valid_dl):
+            images, labels = images.to(device), labels.to(device)
 
-    Leye = torch.rand(bs,3, 16, 16)
-    Reye = torch.rand(bs,3, 16, 16)
-    FaceData = torch.rand(bs,3, 16, 16)  # currently matched with eyes......
+            # Forward pass ➡
+            outputs = model(images)
+            val_loss += loss_func(outputs, labels) * labels.size(0)
 
-    out = model(Leye, Reye, FaceData)
-    print(out.shape)
+            # Compute accuracy and accumulate
+            _, predicted = torch.max(outputs.data, 1)
+            correct += (predicted == labels).sum().item()
+
+            # Log one batch of images to the dashboard, always same batch_idx.
+            if i == batch_idx and log_images:
+                log_image_table(images, predicted, labels, outputs.softmax(dim=1))
+    return val_loss / len(valid_dl.dataset), correct / len(valid_dl.dataset)
+
+
+
+# Create a teble to compare the predicted values versus the true value
+def log_image_table(images, predicted, labels, probs):
+    "Log a wandb.Table with (img, pred, target, scores)"
+    # Create a wandb Table to log images, labels and predictions to
+    table = wandb.Table(
+        columns=["image", "pred", "target"] + [f"score_{i}" for i in range(10)]
+    )
+    for img, pred, targ, prob in zip(
+        images.to("cpu"), predicted.to("cpu"), labels.to("cpu"), probs.to("cpu")
+    ):
+        table.add_data(wandb.Image(img[0].numpy() * 255), pred, targ, *prob.numpy())
+    wandb.log({"predictions_table": table}, commit=False)
+
+# Train your model and upload checkpoints
+# Launch 3 experiments, trying different dropout rates
+for _ in range(3):
+    # initialise a wandb run
+    wandb.init(
+        project="pytorch-intro",
+        config={
+            "epochs": 5,
+            "batch_size": 128,
+            "lr": 1e-3,
+            "dropout": random.uniform(0.01, 0.80),
+        },
+    )
+
+    # Copy your config
+    config = wandb.config
+
+    # Get the data
+    train_dl = get_dataloader(is_train=True, batch_size=config.batch_size)
+    valid_dl = get_dataloader(is_train=False, batch_size=2 * config.batch_size)
+    n_steps_per_epoch = math.ceil(len(train_dl.dataset) / config.batch_size)
+
+    # A simple MLP model
+    # model = get_model(config.dropout)
+
+    # Make the loss and optimizer
+    loss_func = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
+
+    # Training
+    example_ct = 0
+    step_ct = 0
+    for epoch in range(config.epochs):
+        model.train()
+        for step, (images, labels) in enumerate(train_dl):
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+            train_loss = loss_func(outputs, labels)
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+
+            example_ct += len(images)
+            metrics = {
+                "train/train_loss": train_loss,
+                "train/epoch": (step + 1 + (n_steps_per_epoch * epoch))
+                / n_steps_per_epoch,
+                "train/example_ct": example_ct,
+            }
+
+            if step + 1 < n_steps_per_epoch:
+                # Log train metrics to wandb
+                wandb.log(metrics)
+
+            step_ct += 1
+
+        val_loss, accuracy = validate_model(
+            model, valid_dl, loss_func, log_images=(epoch == (config.epochs - 1))
+        )
+
+        # Log train and validation metrics to wandb
+        val_metrics = {"val/val_loss": val_loss, "val/val_accuracy": accuracy}
+        wandb.log({**metrics, **val_metrics})
+
+        # Save the model checkpoint to wandb
+        torch.save(model, "my_model.pt")
+        wandb.log_model(
+            "./my_model.pt",
+            "my_mnist_model",
+            aliases=[f"epoch-{epoch+1}_dropout-{round(wandb.config.dropout, 4)}"],
+        )
+
+        print(
+            f"Epoch: {epoch+1}, Train Loss: {train_loss:.3f}, Valid Loss: {val_loss:3f}, Accuracy: {accuracy:.2f}"
+        )
+
+    # If you had a test set, this is how you could log it as a Summary metric
+    wandb.summary["test_accuracy"] = 0.8
+
+    # Close your wandb run
+    wandb.finish()
+
+
+
+# def train():
+#     model = WholeModel()
+
+#     bs = 16 # batch size
+#     # hidden_size =
+#     # sequence_length =
+#     # num_classes =
+
+#     Leye = torch.rand(bs,3, 16, 16)
+#     Reye = torch.rand(bs,3, 16, 16)
+#     FaceData = torch.rand(bs,3, 16, 16)  # currently matched with eyes......
+
+#     out = model(Leye, Reye, FaceData)
+#     print(out.shape)
 
 
 
